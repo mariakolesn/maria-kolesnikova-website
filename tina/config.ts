41579355615ecd0d21a99f9701ec00c866cbfaaf
mariakolesn <m.koles@yahoo.com>
2026-09-02 14:09:07 +0200

    Add TinaCMS configuration file

---
+++ tina/config.ts
@@ -0,0 +1,50 @@
+import { defineConfig } from "tinacms";
+
+export default defineConfig({
+  branch: "main",
+
+  clientId: "70d23e4b-f8c4-4a4c-9c99-a51653b6907b",
+
+  build: {
+    outputFolder: "admin",
+    publicFolder: ".",
+  },
+
+  schema: {
+    collections: [
+      {
+        name: "page",
+        label: "Website",
+        path: "content",
+        format: "json",
+
+        match: {
+          include: "home",
+        },
+
+        ui: {
+          allowedActions: {
+            create: false,
+            delete: false,
+          },
+        },
+
+        fields: [
+          {
+            name: "headline",
+            label: "Headline",
+            type: "string",
+          },
+          {
+            name: "text",
+            label: "Text",
+            type: "string",
+            ui: {
+              component: "textarea",
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
